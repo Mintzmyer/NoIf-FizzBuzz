175aa6bc31fb74a3a5e3c56e492cd495b159a9f8
Mintzmyer <samimintzmyer@gmail.com>
2017-08-31 16:22:31 -0700

    Skeleton for fizz buzz

---
+++ FizzBuzz.c
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        //  If 3 divides number, print 'Fizz' 
+        //  If 5 divides number print 'Buzz' 
+        //  If both, print 'FizzBuzz'
+        //  If neither, print number
+
+
+
+    }
+
+
+
+    return 0;
+}
+
