ec63b8229730014b2465f2d266b841e3c0a30126
Mintzmyer <samimintzmyer@gmail.com>
2017-08-31 21:00:15 -0700

    While loops...sad face

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -8,6 +8,7 @@
 
 int main()
 {
+    //  Arrays!
     for (int i = 0; i <25; i++)
     {      
         char FizzPrint[3][4] = {"Fizz", "", "",};
@@ -19,9 +20,36 @@ int main()
         printf("%d: %s%s\n", i, FizzPrint[mod3], BuzzPrint[mod5]);  
     }
 
+    //  Ternary operator!
     for (int i = 25; i <50; i++)
     {
         printf("%d: %s%s\n", i, (i%3 == 0) ? "Fizz":"", (i%5 == 0) ? "Buzz":"");
+    }
+
+    //  While loops...ok this one makes me cringe
+    for (int i = 50; i <75; i++)
+    {
+        printf("%d: ", i);
+
+        while (i%3 == 0)
+        {
+            printf("Fizz");
+            break;
+        }
+        while (i%5 == 0)
+        {
+            printf("Buzz");
+            break;
+        }
+        printf("\n");
+    }
+
+    //  
+    for (int i = 75; i < 100; i++)
+    {
+
+
+
     }
     return 0;
 }
