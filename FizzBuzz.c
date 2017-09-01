24d99e316eb2fcf8e134747ab91ab778617cec2a
Mintzmyer <samimintzmyer@gmail.com>
2017-08-31 20:55:28 -0700

    Ternary operators work too

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int main()
-{
-    for (int i = 0; i < 100; i++)
-    {
         //  If 3 divides number, print 'Fizz' 
         //  If 5 divides number print 'Buzz' 
         //  If both, print 'FizzBuzz'
-        //  If neither, print number
-        
+        //  If neither, print number 
+
+int main()
+{
+    for (int i = 0; i <25; i++)
+    {      
         char FizzPrint[3][4] = {"Fizz", "", "",};
         char BuzzPrint[5][4] = {"Buzz", "", "", "", "",};
 
@@ -18,13 +17,12 @@ int main()
         int mod5 = i%5;
  
         printf("%d: %s%s\n", i, FizzPrint[mod3], BuzzPrint[mod5]);  
-        
-
-
     }
 
-
-
+    for (int i = 25; i <50; i++)
+    {
+        printf("%d: %s%s\n", i, (i%3 == 0) ? "Fizz":"", (i%5 == 0) ? "Buzz":"");
+    }
     return 0;
 }
 
