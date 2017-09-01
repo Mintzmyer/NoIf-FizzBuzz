1063cb30e608344b96d4b19e9cae8f3ce441834c
Mintzmyer <samimintzmyer@gmail.com>
2017-08-31 17:39:01 -0700

    Arrays to the rescue

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 
 int main()
@@ -9,7 +10,15 @@ int main()
         //  If 5 divides number print 'Buzz' 
         //  If both, print 'FizzBuzz'
         //  If neither, print number
+        
+        char FizzPrint[3][4] = {"Fizz", "", "",};
+        char BuzzPrint[5][4] = {"Buzz", "", "", "", "",};
 
+        int mod3 = i%3;
+        int mod5 = i%5;
+ 
+        printf("%d: %s%s\n", i, FizzPrint[mod3], BuzzPrint[mod5]);  
+        
 
 
     }
